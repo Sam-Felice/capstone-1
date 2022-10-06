a29350d996317cdc7a4a3c428c124e84d8f8adf0
Samuel Felice <sam.g.felice@outlook.com>
2022-10-06 16:00:30 -0400

    Capstone 1

---
+++ capstone/src/main/java/com/techelevator/BuyerBalance.java
@@ -0,0 +1,21 @@
+package com.techelevator;
+
+import java.math.BigDecimal;
+
+public class BuyerBalance {
+
+
+    private BigDecimal balance;
+
+    public BuyerBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+}
--- capstone/src/main/java/com/techelevator/PurchaseMenu.java
+++ capstone/src/main/java/com/techelevator/PurchaseMenu.java
@@ -2,7 +2,50 @@ package com.techelevator;
 
 public class PurchaseMenu {
 
+    private String menu = "Main menu\n (1) Display vending machine items \n (2) Purchase \n (3) Exit";
+    private String feedMoneyButton = "1";
+    private String selectProductButton = "2";
+    private String finishTransactionButton = "3";
+
+//Constructor
+    public PurchaseMenu(String menu, String feedMoneyButton, String selectProductButton, String finishTransactionButton) {
+        this.menu = menu;
+        this.feedMoneyButton = feedMoneyButton;
+        this.selectProductButton = selectProductButton;
+        this.finishTransactionButton = finishTransactionButton;
+    }
+//Getters
+    public String getMenu() {
+        return menu;
+    }
+
+    public String getFeedMoneyButton() {
+        return feedMoneyButton;
+    }
+
+    public String getSelectProductButton() {
+        return selectProductButton;
+    }
+
+    public String getFinishTransactionButton() {
+        return finishTransactionButton;
+    }
 
 
+//    Setters
+    public void setMenu(String menu) {
+        this.menu = menu;
+    }
 
+    public void setFeedMoneyButton(String feedMoneyButton) {
+        this.feedMoneyButton = feedMoneyButton;
+    }
+
+    public void setSelectProductButton(String selectProductButton) {
+        this.selectProductButton = selectProductButton;
+    }
+
+    public void setFinishTransactionButton(String finishTransactionButton) {
+        this.finishTransactionButton = finishTransactionButton;
+    }
 }
