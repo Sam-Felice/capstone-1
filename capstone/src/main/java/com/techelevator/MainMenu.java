1f705846e46e499d5ca9073a8036cf66d95ac9bc
Mitchell DiPangrazio <dipangraziom@gmail.com>
2022-10-12 09:33:02 -0400

    Some changes

--- capstone/src/main/java/com/techelevator/MainMenu.java
+++ capstone/src/main/java/com/techelevator/MainMenu.java
@@ -1,21 +1,14 @@
 package com.techelevator;
 
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.List;
 
 public class MainMenu {
     //declarations
-    private String displayItems = "1";
-    private String enterPurchaseMenu = "2";
-    private String exitApp = "3";
-    private String menu = "Main menu\n (1) Display vending machine items \n (2) Purchase \n (3) Exit";
-    //constructor
-    public MainMenu(String displayItems, String enterPurchaseMenu, String exitApp) {
-        this.displayItems = displayItems;
-        this.enterPurchaseMenu = enterPurchaseMenu;
-        this.exitApp = exitApp;
-    }
+    final String DISPLAY_ITEMS_BUTTON = "1";
+    final String PURCHASE_MENU_BUTTON = "2";
+    final String EXIT_APP_BUTTON = "3";
+    final String MAIN_MENU_FULL_TEXT = "Main MAIN_MENU_FULL_TEXT\n (1) Display vending machine items \n (2) Purchase \n (3) Exit";
 
     //empty constructor
     public MainMenu() {
@@ -36,20 +29,20 @@ public class MainMenu {
         System.exit(3);
     }
 
-    //method
+    //main menu method
     public void displayMenu(List<Item> items) throws FileNotFoundException {
         boolean isRunning=true;
         while (isRunning) {
-            System.out.println(menu);
+            System.out.println(MAIN_MENU_FULL_TEXT);
             UserInterface ui = new UserInterface();
             String input = ui.getCommands();
-            if (input.equals(displayItems)) {
+            if (input.equals(DISPLAY_ITEMS_BUTTON)) {
                 showItemsDisplay(items);
-            } else if (input.equals(enterPurchaseMenu)) {
-                //display purchase menu
+            } else if (input.equals(PURCHASE_MENU_BUTTON)) {
+                //display purchase MAIN_MENU_FULL_TEXT
                 PurchaseMenu showPurchaseMenu = new PurchaseMenu();
                 showPurchaseMenu.displayPurchaseMenu(items);
-            } else if (input.equals(exitApp)) {
+            } else if (input.equals(EXIT_APP_BUTTON)) {
                 isRunning=false;
                 exitApplication();
             } else {
